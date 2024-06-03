53869225af6b0532202c0f57cadee8bf8e4c36bb
laboratorio <laboratorio@UPEAULA-31710>
2024-06-03 08:45:51 -0500

    Agregar archivos de proyecto.

---
+++ JuanJoseArauzExamenP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JuanJoseArauzExamenP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JuanJoseArauzExamenP2/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace JuanJoseArauzExamenP2
+{
+    public partial class MainPage : ContentPage
+    {
+        string Numero;
+
+        private void Recarga(object sender, EventArgs e)
+        {
+            string Numero = PhoneNumberText.Text;
+            Numero = JuanJoseArauzExamenP2.Celular.ToNumber(Numero);
+        }
+
+        async void EnRecarga(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+       "Confirmacion",
+       "¿Desea recargar la cantidad seleccionada?",
+       "Si",
+       "No"))
+            {
+                await DisplayAlert("Finalizado", "Recarga exitosa", "OK");
+            }
+
+        }
+    }
+
+}
